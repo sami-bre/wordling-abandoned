59fbcfa621d3c824f0bf22855635197da865079c
Samuel Birhanu <samuelbirhanu121@gmail.com>
2022-09-15 11:53:32 +0300

    refractored the code for the search page out of the main.dart file into it's own file: start_page.dart

---
+++ lib/search_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  SearchPageState createState() => SearchPageState();
+}
+
+class SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wordling'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            buildSearchArea(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildSearchArea() {
+  return SizedBox(
+    height: 50.0,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+                label: Text("Define"),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10.0),
+                  bottomLeft: Radius.circular(10.0),
+                ))),
+          ),
+        ),
+        SizedBox(
+          width: 58,
+          height: 50.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(10.0),
+                    bottomRight: Radius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+            child: const Icon(Icons.search),
+          ),
+        ),
+      ],
+    ),
+  );
+}
